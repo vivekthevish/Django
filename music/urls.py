f0abddf2b48b99f16869adbbf4a8a318b87bd28e
Vivek Sharma <vivekthevish@gmail.com>
2017-09-03 12:11:31 +0530

    admin account created and music page modified

--- music/urls.py
+++ music/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
+    #Main Page for Music
     url(r'^$', views.index, name='index'),
+
+    #Main page for music details
+    url(r'^(?P<album_id>[0-9]+)/$',views.details, name='details')
 ]
--- music/views.py
+++ music/views.py
@@ -1,3 +1,6 @@
 from django.http import response
 def index(request):
     return response.HttpResponse("<h1>This is Music HomePage.</h1>")
+
+def details(request,album_id):
+    return response.HttpResponse("<h2>Details for album id : " + str(album_id) + "</h2>")
